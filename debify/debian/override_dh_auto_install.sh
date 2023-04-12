063872a11dacbadd17fdbe212350c9864f057844
Dan Muey <dan@cpanel.net>
2023-04-12 18:18:32 +0000

    ZC-10895: Add support for multiple Ubuntus as well as 22.04

--- debify/debian/override_dh_auto_install.sh
+++ debify/debian/override_dh_auto_install.sh
@@ -3,4 +3,4 @@
 source debian/vars.sh
 
 mkdir -p $DEB_INSTALL_ROOT$_sysconfdir/apt/sources.list.d
-install -m 644 $SOURCE1 $DEB_INSTALL_ROOT$_sysconfdir/apt/sources.list.d/EA4-delayed.list
+install -m 644 EA4-delayed-$OBS_REPO.list $DEB_INSTALL_ROOT$_sysconfdir/apt/sources.list.d/
